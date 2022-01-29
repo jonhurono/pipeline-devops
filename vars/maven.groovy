ea0387f8c3ed0c315e48c4c971d06fafed9c5cab
John C <88783542+jonhurono@users.noreply.github.com>
2022-01-28 21:36:37 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -6,31 +6,39 @@
 
 def call(String pipelineType){
 	
+	figlet 'Maven'
+	
 	if(pipelineType == 'CI'){
 		
-		stage('Compile') {
+	figlet 'Integración Continua'
+		
+	stage('Compile') {
         STAGE = env.STAGE_NAME
         sh "mvn clean compile -e"
+	figlet "Stage: ${env.STAGE_NAME}"
     }
     
     stage('Test') {
         STAGE = env.STAGE_NAME
         sh "mvn clean test -e"
+	figlet "Stage: ${env.STAGE_NAME}"
     }
 
     stage('Jar') {
         STAGE = env.STAGE_NAME
         sh "mvn clean package -e"
-    }
-		
-		
+	figlet "Stage: ${env.STAGE_NAME}"
+    }	
 	} else (){
 		
-		stage('SonarQube analysis') {
+	figlet 'Delivery Continuo'
+		
+	stage('SonarQube analysis') {
         STAGE = env.STAGE_NAME
         def scannerHome = tool 'sonar-scanner';
         withSonarQubeEnv('sonar-server') {
-            bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-maven -Dsonar.sources=src -Dsonar.java.binaries=build"
+        bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-maven -Dsonar.sources=src -Dsonar.java.binaries=build"
+	figlet "Stage: ${env.STAGE_NAME}"
         }
     }
     
@@ -38,10 +46,11 @@ def call(String pipelineType){
         STAGE = env.STAGE_NAME
         echo 'uploadNexus';
         nexusPublisher nexusInstanceId: 'nexus_test', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:/Users/jjcha/Documents/Diplomado/Repos/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+	figlet "Stage: ${env.STAGE_NAME}"
     }
 			
 	}	
-	figlet pipelineType
+
 }
 
 return this;
