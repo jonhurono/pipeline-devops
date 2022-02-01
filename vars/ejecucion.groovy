e842fa509268ef39f0ad39a69184b94f48809802
John Chaves <jjchavescc@gmail.com>
2022-01-31 23:46:26 -0300

    cambios para taller 14 v 15

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -9,7 +9,7 @@ pipeline {
 	}
 
     parameters{
-            choice choices: ['gradle', 'maven'], description: 'Indicar la herramienta de construccion', name: 'buildTool', string(name: 'stage', defaultValue 'Build')
+            choice choices: ['gradle', 'maven'], description: 'Indicar la herramienta de construccion', name: 'buildTool', string(name: 'stage', defaultValue: 'Build')
     }
 
     stages {
